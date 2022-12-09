060523f029b7631d9ab684ecd97d9a7b1fdee5e3
OmarNourEldinMohamed <omarnoureldinkhalil@gmail.com>
2022-12-09 12:59:57 -0800

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function to generate a random number
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
