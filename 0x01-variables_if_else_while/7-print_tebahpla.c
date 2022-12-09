3d72b7cfbcf932f1971c0601e8081cb93433b9ae
OmarNourEldinMohamed <omarnoureldinkhalil@gmail.com>
2022-12-09 13:17:57 -0800

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main function to generate a random number
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
