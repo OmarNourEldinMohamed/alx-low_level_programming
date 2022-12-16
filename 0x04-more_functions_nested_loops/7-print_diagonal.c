0e178c967c79b7472297b9cdef5c77a081740640
OmarNourEldinMohamed <omarnoureldinkhalil@gmail.com>
2022-12-15 17:22:52 -0800

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diangonal
+ * @n: input number
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int co, sp;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (co = 1; c <= n; co++)
+		{
+			for (sp = 1; sp < co; sp++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
