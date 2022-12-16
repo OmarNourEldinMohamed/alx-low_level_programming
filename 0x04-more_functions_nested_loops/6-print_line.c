4290b69ec7339f286ef457cea365c315af37ea80
OmarNourEldinMohamed <omarnoureldinkhalil@gmail.com>
2022-12-15 17:09:21 -0800

    6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,16 +2,17 @@
 
 /**
  * print_line - printing line
- *
+ * @n: input numbers
+ * Return: void
  */
 
-void print_line(void)
+void print_line(int n)
 {
 	int x;
 
 	if (n <= 0)
 		_putchar('\n');
-	for (x = 0; x <= n; x++)
+	for (x = 1; x <= n; x++)
 		_putchar('_');
 	_putchar('\n');
 }
